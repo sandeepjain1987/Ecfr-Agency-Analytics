e0a89538120bf50f960a3c7e14a6a13f34868fab
Sandeep Jain <sandeepjainusa@gmail.com>
2026-01-30 09:20:48 -0800

    Initial commit

---
+++ src/main/java/gov/usds/ecfr/EcfrApplication.java
@@ -0,0 +1,12 @@
+package gov.usds.ecfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcfrApplication {
+//test
+    public static void main(String[] args) {
+        SpringApplication.run(EcfrApplication.class, args);
+    }
+}
